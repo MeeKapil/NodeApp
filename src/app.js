b7b87d167e16f52fdc0a4121074619eecc4dcd35
Kapil Meena <er.kapil.meena@gmail.com>
2023-08-29 20:12:13 -0400

    Initial React App setup commit

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const path = require('path');
 
-// Define a route for the home page
-app.get('/', (req, res) => {
-  res.send('Welcome to my Express web app!');
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define your API routes here
+app.get('/api/data', (req, res) => {
+  res.json({ message: 'Hello from the API!' });
+});
+
+// Serve the React app for all other requests
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Start the server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
